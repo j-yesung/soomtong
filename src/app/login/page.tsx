e05f0233c8c02d97f351c90fb57db58aae047871
j-yesung <hisung-ah@kakao.com>
2025-10-07 22:27:20 +0900

    fix: disable prerender on login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,9 @@
 import { Box } from "@/components/ui";
 import GoogleLoginButton from "@/features/auth/components/googleLoginButton";
 
+export const dynamic = "force-static";
+export const revalidate = 0;
+
 export default function LoginPage() {
   return (
     <Box centerScreen>
