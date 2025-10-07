321b24f32917fb0c68770f21b1819752a926d831
j-yesung <hisung-ah@kakao.com>
2025-10-07 22:32:06 +0900

    fix: login page "revalidate" 제거

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,8 +3,7 @@
 import { Box } from "@/components/ui";
 import GoogleLoginButton from "@/features/auth/components/googleLoginButton";
 
-export const dynamic = "force-static";
-export const revalidate = 0;
+export const dynamic = "force-dynamic";
 
 export default function LoginPage() {
   return (
--- src/features/auth/components/googleLoginButton/index.tsx
+++ src/features/auth/components/googleLoginButton/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import styled from "styled-components";
 
 import { GoogleLogo } from "@/assets/svg/logo";
