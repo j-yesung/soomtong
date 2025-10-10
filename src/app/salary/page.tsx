152bd2f84267fd1c884f6f94f4de34a62624b832
j-yesung <hisung-ah@kakao.com>
2025-10-10 15:22:18 +0900

    feat: 월수입 페이지 헤더 컴포넌트로 분리

--- src/app/salary/page.tsx
+++ src/app/salary/page.tsx
@@ -2,10 +2,11 @@
 
 import { useEffect } from "react";
 
-import { Column, Heading } from "@/components/ui";
+import { Column } from "@/components/ui";
 import { useUserQuery } from "@/features/auth/queries";
 import { useAuthStore } from "@/features/auth/store";
 import SalaryForm from "@/features/salary/components/salaryForm";
+import SalaryHeader from "@/features/salary/components/salaryHeader";
 
 export default function SalaryPage() {
   const updateUserInfo = useAuthStore((state) => state.updateUserInfo);
@@ -19,14 +20,7 @@ export default function SalaryPage() {
 
   return (
     <Column gap={40} fullWidth>
-      <Column as="header">
-        <Heading level={2} fontWeight="bold">
-          월급을 입력해 주세요
-        </Heading>
-        <Heading level={5} fontWeight="normal" color="secondary">
-          월 수입을 기반으로 예산을 계획해 보세요
-        </Heading>
-      </Column>
+      <SalaryHeader />
       <SalaryForm />
     </Column>
   );
---
+++ src/features/salary/components/salaryHeader/index.tsx
@@ -0,0 +1,14 @@
+import { Column, Heading } from "@/components/ui";
+
+export default function SalaryHeader() {
+  return (
+    <Column as="header">
+      <Heading level={2} fontWeight="bold">
+        월급을 입력해 주세요
+      </Heading>
+      <Heading level={5} fontWeight="normal" color="secondary">
+        월수입을 기반으로 생활비를 계획해 보세요
+      </Heading>
+    </Column>
+  );
+}
