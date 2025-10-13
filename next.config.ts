d2df35b5c5d502224e63e11d31178f44760ac8dc
j-yesung <hisung-ah@kakao.com>
2025-10-13 15:41:54 +0900

    fix: workflows yml 수정

--- next.config.ts
+++ next.config.ts
@@ -8,12 +8,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
