e71aa3322cbbd5ce557c4739d93cc789992f1e23
j-yesung <hisung-ah@kakao.com>
2025-10-26 18:51:31 +0900

    style: flex, column, row display 속성 고정

--- src/components/ui/column/index.tsx
+++ src/components/ui/column/index.tsx
@@ -4,6 +4,6 @@ import Flex, { FlexProps } from "../flex";
 
 export type ColumnProps = React.ComponentProps<typeof Column>;
 
-export default function Column(props: Omit<FlexProps, "direction">) {
-  return <Flex direction="column" {...props} />;
+export default function Column(props: Omit<FlexProps, "direction" | "display">) {
+  return <Flex direction="column" display="flex" {...props} />;
 }
