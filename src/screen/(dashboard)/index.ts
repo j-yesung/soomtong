4871a3a9580a2d711d4e58fbb9c5a82c8037d7a4
j-yesung <hisung-ah@kakao.com>
2025-10-27 19:38:02 +0900

    move: 폴더 그룹핑

---
+++ src/screen/(dashboard)/index.ts
@@ -0,0 +1,4 @@
+"use client";
+
+export { default as BudgetBoardScreen } from "./dashboard/budgetBoardScreen";
+export { default as FixedExpenseBoardScreen } from "./dashboard/fixedExpenseBoardScreen";
--- src/screen/dashboard/index.ts
+++
@@ -1,4 +0,0 @@
-"use client";
-
-export { default as BudgetBoardScreen } from "./budgetBoardScreen";
-export { default as FixedExpenseBoardScreen } from "./fixedExpenseBoardScreen";
