2f095ff6f1702306d05d355e76c22151c7dfe134
j-yesung <hisung-ah@kakao.com>
2025-12-16 22:31:27 +0900

    fix: middleware 및 store-hydrator 수정

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,3 +1,5 @@
+import { redirect } from "next/navigation";
+
 import StoreHydrator from "@/app/(protected)/store-hydrator";
 import { createClient } from "@/lib/supabase/server";
 
@@ -7,13 +9,13 @@ export default async function ProtectedLayout({ children }: { children: React.Re
   const {
     data: { session },
   } = await supabase.auth.getSession();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+
+  const user = session?.user;
+  if (!user) redirect("/login");
 
   return (
     <>
-      {session ? <StoreHydrator user={user!} /> : null}
+      <StoreHydrator user={user} />
       {children}
     </>
   );
