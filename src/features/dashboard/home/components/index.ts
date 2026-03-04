938d713b077264c39b8d17aee8aa5ed1dc7a7597
j-yesung <hisung-ah@kakao.com>
2026-03-04 17:41:16 +0900

    remove: 사용하지 않는 파일 제거

--- src/features/dashboard/home/components/index.ts
+++ src/features/dashboard/home/components/index.ts
@@ -1,10 +1,9 @@
 "use client";
 
-export { default as FixedExpenseReportSkeleton } from "./skeleton/fixedExpenseReportSkeleton";
+export { default as BudgetBoard } from "./budgetBoard";
 export { default as BudgetBarChart } from "./budgetRatioCard/budgetBarChart";
 export { default as BudgetReport } from "./budgetRatioCard/budgetReport";
+export { default as DashboardContent } from "./dashboardContent";
+export { default as FixedExpenseBoard } from "./fixedExpenseBoard";
 export { default as FixedExpenseDonutChart } from "./fixedExpenseCard/fixedExpenseDonutChart";
 export { default as FixedExpenseReport } from "./fixedExpenseCard/fixedExpenseReport";
-export { default as BudgetBoard } from "./budgetBoard";
-export { default as FixedExpenseBoard } from "./fixedExpenseBoard";
-export { default as DashboardContent } from "./dashboardContent";
